356072da5334db446e9dcfe0b159f023dbbf96d1
FatihG34 <fthgnydn34@gmail.com>
2022-06-17 09:44:52 +0300

    async-await worked

--- app.js
+++ app.js
@@ -46,40 +46,62 @@ const todos = [
 let todoListEl = document.querySelector("#todoList");
 
 
-function todoList() {
-    setTimeout(() => {
-        todoItems = "";
-        todos.forEach(item => {
-            todoItems += `<li>
-                            <b>${item.title}</b>
-                            <p>${item.description}</p>
-                          </li>`
-        });
-        todoListEl.innerHTML = todoItems;
-    }, 1000);
-};
-function newTodo(todo) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            todos.push(todo);
-            const err = false;
-            if (!err) {
-                resolve(todos);
-            } else {
-                reject("Have an Error!!!...");
-            }
-        }, 1500);
-    })
+// function todoList() {
+//     setTimeout(() => {
+//         todoItems = "";
+//         todos.forEach(item => {
+//             todoItems += `<li>
+//                             <b>${item.title}</b>
+//                             <p>${item.description}</p>
+//                           </li>`
+//         });
+//         todoListEl.innerHTML = todoItems;
+//     }, 1000);
+// };
+// function newTodo(todo) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             todos.push(todo);
+//             const err = false;
+//             if (!err) {
+//                 resolve(todos);
+//             } else {
+//                 reject("Have an Error!!!...");
+//             }
+//         }, 1500);
+//     })
 
-};
-newTodo({
-    title: "todo 4",
-    description: "kendine vakit ayır..."
-}).then((response) => {
-    console.log("New Array :", response);
-    todoList();
-}).catch((e) => {
-    console.log(e);
-})
+// };
+// newTodo({
+//     title: "todo 4",
+//     description: "kendine vakit ayır..."
+// }).then((response) => {
+//     console.log("New Array :", response);
+//     todoList();
+// }).catch((e) => {
+//     console.log(e);
+// })
 
-todoList();
+// todoList();
+
+
+
+//* async-await practice
+
+async function fetchPost() {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const data = await response.json();
+    console.log(data);
+    data.forEach(i => {
+        console.log("i", i);
+    });
+};
+fetchPost();
+
+const ulkeler = async () => {
+    const ulkeList = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const ulke = await ulkeList.json();
+    console.log(ulke[0]);
+};
+
+ulkeler();
